116f7de0fbcbf266e0abf550f4321bc265b52c2f
ALeX <aatevs@gmail.com>
2024-02-16 13:22:23 +0300

    tags

--- visual/posts/models.py
+++ visual/posts/models.py
@@ -14,7 +14,7 @@ class Like(models.Model):
 
 class Post(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название поста')
-    image = models.CharField(max_length=50, verbose_name='Изображение')
+    image = models.ImageField(max_length=50, verbose_name='Изображение')
     text = models.TextField(blank=True, verbose_name='Описание')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     slug = models.CharField(max_length=200, unique=True, verbose_name='Слаг')
